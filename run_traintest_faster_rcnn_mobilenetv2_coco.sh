4eeabb585b454235f01e0ba8a25b5cd128a147bb
zengsn <shaoning.zeng@gmail.com>
2022-05-19 13:31:00 +0800

    Shell to train and test faster_rcnn_mobilenetv2 on COCO

--- run_traintest_faster_rcnn_mobilenetv2_coco.sh
+++ run_traintest_faster_rcnn_mobilenetv2_coco.sh
@@ -7,28 +7,48 @@
 # configs/mask_rcnn/mask_rcnn_mobilnetv2_coco.py 
 # configs/centernet/centernet_mobilenetv2_dcnv2_140e_coco.py
 
+INPUT_SHAPE="1333 800"
 MODEL_NAME="faster_rcnn"
 CONF_FNAME="faster_rcnn_mobilenetv2_coco"
 
 # Train the models one by one
-python tools/train.py \
+WORK_DIR=train_${CONF_FNAME}
+PTH_FILE=${WORK_DIR}/epoch_12.pth
+if [ -f "$PTH_FILE" ]; then
+    echo "Train result exist in ${PTH_FILE}, skip training."
+else
+    python tools/train.py \
 	configs/${MODEL_NAME}/${CONF_FNAME}.py \
-	--work-dir train_${CONF_FNAME} \
+	--work-dir ${WORK_DIR} \
 	--auto-resume
+fi 
 
-# INPUT_SHAPE="1333 800"
+# Get flops
+echo "Get model complexity in flops: "
+python tools/analysis_tools/get_flops.py \
+	configs/${MODEL_NAME}/${CONF_FNAME}.py \
+	--shape ${INPUT_SHAPE} > ${WORK_DIR}_flops.txt
+cat ${WORK_DIR}_flops.txt
 
 # Test: Generate results in .pkl
-RESULT_PKL=train_${CONF_FNAME}_results.pkl
+RESULT_PKL=${WORK_DIR}_results.pkl
 if [ -f "$RESULT_PKL" ]; then
     echo "Results exist in ${RESULT_PKL}."
 else 
     echo "Test to get results: "
     python tools/test.py \
 	configs/${MODEL_NAME}/${CONF_FNAME}.py \
-	train_${CONF_FNAME}/epoch_12.pth \
+	${PTH_FILE} \
 	--out ${RESULT_PKL} 
     echo "Results saved to ${RESULT_PKL}. "
 fi
+
+# Result analysis
+echo "Analyze the results: "
+python tools/analysis_tools/analyze_results.py \
+	configs/${MODEL_NAME}/${CONF_FNAME}.py \
+	${RESULT_PKL} \
+	${WORK_DIR}_results_figs \
+	--topk 50
 
 
